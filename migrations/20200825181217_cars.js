79de5ab5e4691e12107816547dc06f2fb07eecc0
Andre Freeman <asf@mail.usf.edu>
2020-08-25 18:31:19 -0400

    Created cars.db3

--- migrations/20200825181217_cars.js
+++ migrations/20200825181217_cars.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema.createTable("cars", (table) => {
+        table.increments("id")
+        table.text("VIN").notNull().unique()
+        table.text("make").notNull()
+        table.real("mileage").notNull()
+        table.text("transmission type").defaultTo("unknown")
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExist("cars")
 };
